642f4b41d72bb431b6026493ddaa15588386bb25
Digvijay Singh <dssodha1234@gmail.com>
2023-04-03 10:50:48 +0530

    Make Grid dynamic to user input,UI for reset and start
    Grid can be made with user input while in same area
    and it can also be reset and start

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
 function gridMaker() {
+    let noOfRows;
+    do {
+        noOfRows = prompt("enter no. of grid between 1 to 100", 50);
+    }
+    while (!(noOfRows <= 100 && noOfRows >= 1))
+
     const allGridRow = document.querySelector("#grids");
-    for (let i = 1; i <= 16; i++) {
+    for (let i = 1; i <= noOfRows; i++) {
         //make container
         const containerForGridRow = document.createElement("div");
         containerForGridRow.classList.add(`grid-row${i}`);
-        for (let j = 1; j <= 16; j++) {
-            //add 16 div with 16 by 16 dimension
+
+        for (let j = 1; j <= noOfRows; j++) {
+            //add  div with 16 by 16 dimension
             const gridElement = document.createElement("div");
             gridElement.classList.add("atomGridElement");
-            // gridElement.style.cssText = "height:16px;width:16px;background-color:red";
+            const availablePixelArea = 460;
+            const pixelSizeForEachGridElement = availablePixelArea / noOfRows;
+
+            //add pixel size dynamically to element based on noOfRows
+            gridElement.style.cssText = `height:${pixelSizeForEachGridElement}px;width:${pixelSizeForEachGridElement}px`;
             containerForGridRow.append(gridElement);
         }
         allGridRow.append(containerForGridRow);
     }
 }
-gridMaker();
-const allGridAtoms = document.querySelectorAll(".atomGridElement");
-allGridAtoms.forEach(atomGridElement => {
-    atomGridElement.addEventListener("mouseover", (Event) => {
-        atomGridElement.style.backgroundColor = "red";
-    });
-});
 
-gridMaker();
+function draw() {
+    const allGridAtoms = document.querySelectorAll(".atomGridElement");
+    console.log(allGridAtoms);
+    allGridAtoms.forEach(atomGridElement => {
+        atomGridElement.addEventListener("mouseover", (Event) => {
+            atomGridElement.style.backgroundColor = "red";
+        });
+    });
+}
+
+
+function resetGame() {
+    const grid = document.querySelector("#grids");
+    grid.innerHTML = '';
+    gridMaker();
+    draw();
+}
+//to reset
+document.querySelector("#Reset").addEventListener("click", resetGame);
+
+//to start
+document.querySelector("#Start").addEventListener("click", () => {
+    resetGame();
+});
