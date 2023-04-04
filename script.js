280972ae896f81cfff7b75096307e88a511798a0
Digvijay Singh <dssodha1234@gmail.com>
2023-04-04 12:49:53 +0530

    Create color toggle option and refactor code
    Now user can toggle between two colors, either can choose bold or
    rainbow like color to draw

--- script.js
+++ script.js
@@ -19,34 +19,54 @@ function gridMaker() {
             const pixelSizeForEachGridElement = availablePixelArea / noOfRows;
 
             //add pixel size dynamically to element based on noOfRows
-            gridElement.style.cssText = `height:${pixelSizeForEachGridElement}px;width:${pixelSizeForEachGridElement}px`;
+            gridElement.style.cssText = `height:${pixelSizeForEachGridElement}px;width:${pixelSizeForEachGridElement}px;`;
             containerForGridRow.append(gridElement);
         }
         allGridRow.append(containerForGridRow);
     }
 }
+function getBoldColor() {
+    return "black";
+}
+function getRandomColor() {
+    let colorRGB = [];
+    for (let i = 0; i < 3; i++) {
+        colorRGB.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + colorRGB.join(',') + ')';
+}
 
-function draw() {
+function draw(colorTypeFunc) {
     const allGridAtoms = document.querySelectorAll(".atomGridElement");
-    console.log(allGridAtoms);
+    let colorOptions = colorTypeFunc;
+    console.log(colorOptions);
     allGridAtoms.forEach(atomGridElement => {
-        atomGridElement.addEventListener("mouseover", (Event) => {
-            atomGridElement.style.backgroundColor = "red";
+        atomGridElement.addEventListener("mouseover", () => {
+            atomGridElement.style.backgroundColor = colorOptions();
         });
     });
 }
-
 
 function resetGame() {
     const grid = document.querySelector("#grids");
     grid.innerHTML = '';
     gridMaker();
-    draw();
 }
-//to reset
-document.querySelector("#Reset").addEventListener("click", resetGame);
 
 //to start
+resetGame();
 document.querySelector("#Start").addEventListener("click", () => {
     resetGame();
+});
+
+const colorOptions = document.querySelectorAll('#drawOptions>*');
+colorOptions.forEach(eachColorOption => {
+    eachColorOption.addEventListener('click', (event) => {
+        if (event.target.id === "boldColor") {
+            draw(getBoldColor);
+        }
+        else if (event.target.id === "rainbow") {
+            draw(getRandomColor);
+        }
+    })
 });
